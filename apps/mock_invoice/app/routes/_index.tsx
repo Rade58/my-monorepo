1fd8e3a84e3cd01f21ad1361b2ada9a443a66081
Rade58 <bajic.rade2@gmail.com>
2023-06-10 03:27:11 +0200

    generic-6

--- apps/mock_invoice/app/routes/_index.tsx
+++ apps/mock_invoice/app/routes/_index.tsx
@@ -1,11 +1,96 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
 
+import { Form, Link, NavLink, Outlet, useNavigation } from "@remix-run/react";
 import { useOptionalUser } from "~/utils";
+import clsx from "clsx";
+import { useSpinDelay } from "spin-delay";
 
-export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
+import {
+  FullFakebooksLogo,
+  LogoutIcon,
+  SpinnerIcon,
+  UpRightArrowIcon,
+} from "~/components/common";
 
-export default function Index() {
+export const meta: V2_MetaFunction = () => [{ title: "Mock Invoice" }];
+
+export default function AppRoute() {
+  const navg = useNavigation();
+  const showSpinner = useSpinDelay(navg.state !== "idle", {
+    delay: 200,
+    minDuration: 300,
+  });
+  return (
+    <div className="relative flex h-full rounded-lg bg-white text-gray-600">
+      <div className="border-r border-gray-100 bg-gray-50">
+        <div className="p-4">
+          <div className="flex flex-wrap items-center gap-1">
+            <Link to=".">
+              <FullFakebooksLogo size="sm" position="left" />
+            </Link>
+            <Spinner visible={showSpinner} />
+          </div>
+          <div className="h-7" />
+          <div className="flex flex-col font-bold text-gray-800">
+            <NavItem to="dashboard">Dashboard</NavItem>
+            <NavItem to="accounts">Accounts</NavItem>
+            <NavItem to="sales">Sales</NavItem>
+            <NavItem to="expenses">Expenses</NavItem>
+            <NavItem to="reports">Reports</NavItem>
+            <a
+              href="https://github.com/FrontendMasters/advanced-remix"
+              className="my-1 flex gap-1 py-1 px-2 pr-16 text-[length:14px]"
+            >
+              GitHub <UpRightArrowIcon />
+            </a>
+
+            <Form
+              method="post"
+              action="/logout"
+              className="my-1 py-1 px-2 pr-16 text-[length:14px]"
+            >
+              <button type="submit" className="flex gap-1 font-bold">
+                Logout <LogoutIcon />
+              </button>
+            </Form>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+function NavItem({ to, children }: { to: string; children: React.ReactNode }) {
+  return (
+    <NavLink
+      to={to}
+      prefetch="intent"
+      className={({ isActive }) =>
+        `my-1 py-1 px-2 pr-16 text-[length:14px] ${
+          isActive ? "rounded-md bg-gray-100" : ""
+        }`
+      }
+    >
+      {children}
+    </NavLink>
+  );
+}
+
+function Spinner({ visible }: { visible: boolean }) {
+  return (
+    <SpinnerIcon
+      className={clsx("animate-spin transition-opacity", {
+        "opacity-0": !visible,
+        "opacity-100": visible,
+      })}
+    />
+  );
+}
+
+/* export default function Index() {
   const user = useOptionalUser();
   return (
     <main className="bg-base-100 relative min-h-screen sm:flex sm:items-center sm:justify-center">
@@ -140,3 +225,4 @@ export default function Index() {
     </main>
   );
 }
+ */
