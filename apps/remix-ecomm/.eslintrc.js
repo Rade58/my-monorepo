285619bf1259755a0c171d8052068a1df8a7f0c9
Rade58 <bajic.rade2@gmail.com>
2023-06-21 10:57:46 +0200

    generic-6

---
+++ apps/remix-ecomm/.eslintrc.js
@@ -0,0 +1,6 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["custom"],
+  // extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+};
