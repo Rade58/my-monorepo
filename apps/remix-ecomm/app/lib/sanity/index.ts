0bf9baf01a85b8f70bbad75e2fcec38da8489620
Rade58 <bajic.rade2@gmail.com>
2023-06-22 03:38:39 +0200

    generic-6

--- apps/remix-ecomm/app/lib/sanity/index.ts
+++ apps/remix-ecomm/app/lib/sanity/index.ts
@@ -7,7 +7,7 @@ const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET_NAME,
   useCdn: true, // set to `true` to fetch from edge cache
-  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+  apiVersion: '2023-06-22', // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
 });
 
