4b4ab28bdf6da85a6f9a266cf6604c8f88c39509
Rade58 <bajic.rade2@gmail.com>
2023-06-25 10:32:35 +0200

    generic-6

--- apps/remix-ecomm/app/stores/cartStore.ts
+++ apps/remix-ecomm/app/stores/cartStore.ts
@@ -28,11 +28,47 @@ interface State {
 interface Actions {
   addToCart: (product: CartProduct) => void;
   removeFromCart: (productId: string) => void;
+  toggleShowCart: () => void;
 }
 
-export const useCart = create<State>((set) => ({
+export const useCart = create<State & Actions>((set, get) => ({
+  addToCart(product) {
+    set((state) => {
+      return {
+        cart: [...state.cart, product],
+        totalItems: state.totalItems + 1,
+        totalPrice: state.totalPrice + product.price,
+      };
+    });
+  },
+  removeFromCart(productId) {
+    set((state) => {
+      let price: number = 0;
+
+      const products = state.cart.filter((prod) => {
+        if (prod.id !== productId) {
+          return true;
+        }
+
+        price = prod.price;
+
+        return false;
+      });
+
+      return {
+        cart: products,
+        totalPrice: state.totalPrice - price,
+        totalItems: state.totalItems - 1,
+      };
+    });
+  },
+  toggleShowCart() {
+    set((state) => {
+      return { showCart: !state.showCart };
+    });
+  },
   cart: [],
+  showCart: false,
   totalItems: 0,
   totalPrice: 0,
-  showCart: false,
 }));
