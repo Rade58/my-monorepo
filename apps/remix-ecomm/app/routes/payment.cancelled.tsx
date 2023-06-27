edf31e24f93ef801b9f0abf7e20a6d790e53a8e6
Rade58 <bajic.rade2@gmail.com>
2023-06-27 18:22:35 +0200

    feat(remix ecomm app): styled payment success and payment cancellation page

--- apps/remix-ecomm/app/routes/payment.cancelled.tsx
+++ apps/remix-ecomm/app/routes/payment.cancelled.tsx
@@ -1,3 +1,38 @@
 export default function PaymentCancelled() {
-  return <div>Payment Cancelled</div>;
+  return (
+    <div className="h-[86vh] flex justify-center items-center">
+      <div className="p-6 md:mx-auto">
+        <div className="flex justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-16 h-16 text-error"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </div>
+
+        <div className="text-center">
+          <h3 className="md:text-2xl text-base font-semibold text-center">
+            Payment Cancelled!
+          </h3>
+          <p className="my-2">You won{"'"}t get charged.</p>
+          <p> Have a great day! </p>
+          {/* <div className="py-10 text-center">
+            <Link to="/" className="btn btn-primary">
+              GO BACK
+            </Link>
+              </div>
+        */}
+        </div>
+      </div>
+    </div>
+  );
 }
