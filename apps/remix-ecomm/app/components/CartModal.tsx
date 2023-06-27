4197c18c032a74ecf317b952727229d5e001ab8c
Rade58 <bajic.rade2@gmail.com>
2023-06-27 17:44:15 +0200

    generic-6

--- apps/remix-ecomm/app/components/CartModal.tsx
+++ apps/remix-ecomm/app/components/CartModal.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, /* useRef, */ useState } from "react";
 import { useCart } from "~/stores/cartStore";
 
 import { Dialog, Transition } from "@headlessui/react";
@@ -72,7 +72,7 @@ export default function CartModal() {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-base-200 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-hidden">
@@ -88,10 +88,10 @@ export default function CartModal() {
                 leaveTo="translate-x-full"
               >
                 <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                  <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-base-200 shadow-xl">
                     <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                       <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-lg font-medium text-gray-900">
+                        <Dialog.Title className="text-lg font-medium">
                           Shopping cart
                         </Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
@@ -110,7 +110,7 @@ export default function CartModal() {
                         <div className="flow-root">
                           <ul
                             role="list"
-                            className="-my-6 divide-y divide-gray-200"
+                            className="-my-6 divide-y divide-secondary-focus"
                           >
                             {cart.map((product) => (
                               <li key={product.id} className="flex py-6">
@@ -124,7 +124,7 @@ export default function CartModal() {
 
                                 <div className="ml-4 flex flex-1 flex-col">
                                   <div>
-                                    <div className="flex justify-between text-base font-medium text-gray-900">
+                                    <div className="flex justify-between text-base font-medium text-secondary">
                                       <h3>
                                         <Link
                                           to={`/product/${product.slug.current}`}
@@ -139,9 +139,7 @@ export default function CartModal() {
                                     </p> */}
                                   </div>
                                   <div className="flex flex-1 items-end justify-between text-sm">
-                                    <p className="text-gray-500">
-                                      Qty {product.quantity}
-                                    </p>
+                                    <p className="">Qty {product.quantity}</p>
 
                                     <div className="flex">
                                       <button
@@ -152,7 +150,7 @@ export default function CartModal() {
                                           }
                                         }}
                                         type="button"
-                                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                                        className="font-medium text-error hover:text-error-content"
                                       >
                                         {product.quantity > 1
                                           ? "Remove one"
@@ -168,8 +166,8 @@ export default function CartModal() {
                       </div>
                     </div>
 
-                    <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                      <div className="flex justify-between text-base font-medium text-gray-900">
+                    <div className="border-t border-primary-focus px-4 py-6 sm:px-6">
+                      <div className="flex justify-between text-base font-medium text-secondary">
                         <p>Subtotal</p>
                         <p>
                           {totalPrice.toLocaleString("en-US", {
@@ -178,7 +176,7 @@ export default function CartModal() {
                           })}
                         </p>
                       </div>
-                      <p className="mt-0.5 text-sm text-gray-500">
+                      <p className="mt-0.5 text-sm">
                         Shipping and taxes calculated at checkout.
                       </p>
                       <form className="mt-6" action="/buy" method="POST">
@@ -187,7 +185,7 @@ export default function CartModal() {
                           //   setSubmitStatus(true);
                           // }}
                           // type="submit"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          className="btn btn-primary btn-block"
                           disabled={state !== "idle"}
                         >
                           Checkout
@@ -200,10 +198,10 @@ export default function CartModal() {
                       </form>
                       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                         <p>
-                          or
+                          or{" "}
                           <button
                             type="button"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                            className="font-medium text-primary hover:text-secondary"
                             onClick={() => toggleShowCart()}
                           >
                             Continue Shopping
