1254cc8875dd8417922e65c7aec99ddfee4d0527
Rade58 <bajic.rade2@gmail.com>
2023-07-11 13:37:48 +0200

    generic-6

--- apps/cinesvelte/src/routes/+page.ts
+++ apps/cinesvelte/src/routes/+page.ts
@@ -1,28 +1,32 @@
-import type {LoadEvent} from '@sveltejs/kit'
-import type { MovieDetails, MovieList } from '$lib/types';
-import * as api from '$lib/api';
-
+import type { LoadEvent } from "@sveltejs/kit";
+import type { MovieDetails, MovieList } from "$lib/types";
+import * as api from "$lib/api";
 
 // const BASE = "https://api.movies.tastejs.com"
 
-
-export async function load({fetch} : LoadEvent){
-  // const response = await fetch(BASE + '/trending/movie/day') 
+export async function load({ fetch }: LoadEvent) {
+  // const response = await fetch(BASE + '/trending/movie/day')
 
   // const trending = await response.json() as MovieList;
-  const trending = await api.get(fetch,'/trending/movie/day') as MovieList
+  const trending = (await api.get(fetch, "/trending/movie/day")) as MovieList;
+  const now_playing = (await api.get(
+    fetch,
+    "/trending/now_playing"
+  )) as MovieList;
+  const upcomming = (await api.get(fetch, "/trending/upcomming")) as MovieList;
 
-  const featured = trending.results[0]
+  const featured = trending.results[0];
 
-  const featured_data = await api.get(fetch, '/movie/' + featured.id, {
-    append_to_response: 'images'
-  }) as MovieDetails
+  const featured_data = (await api.get(fetch, "/movie/" + featured.id, {
+    append_to_response: "images",
+  })) as MovieDetails;
 
   // const featured_data = await featured_response.json() as MovieDetails
 
   return {
     trending,
-    featured_data
-  }
-
+    featured_data,
+    now_playing,
+    upcomming,
+  };
 }
