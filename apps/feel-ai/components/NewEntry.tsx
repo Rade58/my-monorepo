b1223339ec68db669be4571b29329cb930b3ae84
Rade58 <bajic.rade2@gmail.com>
2023-07-16 16:29:19 +0200

    generic-6

--- apps/feel-ai/components/NewEntry.tsx
+++ apps/feel-ai/components/NewEntry.tsx
@@ -6,18 +6,16 @@ export default function NewEntry() {
   const router = useRouter();
 
   const handleOnClick = async () => {
+    console.log("New Entry");
     //
     // router.push(`/journal/${data.id}`)
   };
 
   return (
-    <div
-      className="cursor-pointer overflow-hidden rounded-lg bg-base-300 shadow"
-      onClick={handleOnClick}
-    >
-      <div className="px-4 py-5 sm:p-6">
-        <span className="text-3xl">New Entry</span>
-      </div>
+    <div>
+      <button className="btn btn-primary btn-lg" onClick={handleOnClick}>
+        New Entry
+      </button>
     </div>
   );
 }
