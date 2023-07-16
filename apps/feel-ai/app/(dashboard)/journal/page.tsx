3f6847e486a497835666a73f9443f29d57761b21
Rade58 <bajic.rade2@gmail.com>
2023-07-16 15:01:57 +0200

    generic-6

--- apps/feel-ai/app/(dashboard)/journal/page.tsx
+++ apps/feel-ai/app/(dashboard)/journal/page.tsx
@@ -1,16 +1,39 @@
 import { prisma } from "@/lib/db";
+import { getUserByClerkId } from "@/util/auth";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function getEntries() {
+  const user = await getUserByClerkId({});
+  /* if (user && "journalEntries" in user) {
+    return user.journalEntries;
+  } */
+
+  if (user) {
+    const entries = await prisma.feelJournalEntry.findMany({
+      where: {
+        user: {
+          id: user.id,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return entries;
+  }
+
+  return [];
+}
 
 // if user is not sign in he will be redirected from this page
 // to sign in page, since we set that up inside middleware
 
 export default async function Journal() {
-  /* const neuUser = await prisma.feelUser.create({
-    data: {
-      name: "Joe",
-    },
-  });
+  const entries = await getEntries();
 
-  return <div>{neuUser.id}</div>; */
+  console.log({ entries });
 
-  return <div>Hello Journal</div>;
+  return <div>{entries.length}</div>;
 }
---
+++ apps/feel-ai/util/auth/index.ts
@@ -0,0 +1,76 @@
+import { prisma } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+
+export const getUserByClerkId = async ({
+  include = {},
+  select = {},
+}: {
+  include?: {
+    _count?: boolean;
+    journalEntries?: boolean;
+  };
+  select?: {
+    _count?: boolean;
+    clerkId?: boolean;
+    createdAt?: boolean;
+    email?: boolean;
+    id?: boolean;
+    journalEntries?: boolean;
+    updatedAt?: boolean;
+  };
+}) => {
+  try {
+    const { userId } = auth();
+    console.log({ clerkId: userId });
+    let isSelect = false;
+    let isInclude = false;
+
+    if (Object.entries(select).length > 0) {
+      isSelect = true;
+    }
+    if (Object.entries(include).length > 0) {
+      isInclude = true;
+    }
+
+    if (isSelect && isInclude) {
+      throw new Error("You can use both select and include when querying");
+    }
+
+    if (isSelect) {
+      const user = await prisma.feelUser.findUniqueOrThrow({
+        where: {
+          clerkId: userId,
+        },
+        select,
+      });
+
+      console.log({ user });
+
+      return user;
+    }
+
+    if (isInclude) {
+      const user = await prisma.feelUser.findUniqueOrThrow({
+        where: {
+          clerkId: userId,
+        },
+        include: {
+          ...include,
+        },
+      });
+      console.log({ user });
+
+      return user;
+    }
+
+    return prisma.feelUser.findUniqueOrThrow({
+      where: {
+        clerkId: userId,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+
+    return null;
+  }
+};
