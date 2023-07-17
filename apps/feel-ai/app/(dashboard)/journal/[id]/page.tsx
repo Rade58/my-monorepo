3fefc139965f088d87cf23f4738bd50fc1888ec2
Rade58 <bajic.rade2@gmail.com>
2023-07-17 16:27:25 +0200

    fix(db_two) compound index on feel entry

--- apps/feel-ai/app/(dashboard)/journal/[id]/page.tsx
+++ apps/feel-ai/app/(dashboard)/journal/[id]/page.tsx
@@ -1,7 +1,50 @@
-export default function JournalEntryPage({
+import Editor from "@/components/Editor";
+import { prisma } from "@/lib/db";
+import { getUserByClerkId } from "@/util/auth";
+
+async function getEntryById(id: string) {
+  const user = await getUserByClerkId({});
+
+  // if we didn't have compound index in the schema
+  /* const entry = prisma.feelJournalEntry.findFirst({
+    where: {
+      AND: {
+        id,
+        userId: user.id,
+      },
+    },
+    include: {
+      feelAnalysis: true,
+    },
+  }); */
+
+  // since I added compound index in the schema
+  // we can use findUnique method
+  const entry = await prisma.feelJournalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+    include: {
+      feelAnalysis: true,
+    },
+  });
+
+  return entry;
+}
+
+export default async function JournalEntryPage({
   params,
 }: {
   params: { id: string };
 }) {
-  return <div>Entry {params.id}</div>;
+  const entry = await getEntryById(params.id);
+
+  return (
+    <div>
+      <Editor entry={entry} />
+    </div>
+  );
 }
---
+++ apps/feel-ai/components/Editor.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import type { FeelJournalEntry, FeelAnalysis } from "db_two";
+
+export default function Editor({
+  entry,
+}: {
+  entry: FeelJournalEntry & { feelAnalysis?: FeelAnalysis };
+}) {
+  return null;
+}
