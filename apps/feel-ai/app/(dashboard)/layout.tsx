16b26ee04c69274536bec1196a5986f1aa506290
Rade58 <bajic.rade2@gmail.com>
2023-07-19 18:16:29 +0200

    feat(feel-ai app) nav menu

--- apps/feel-ai/app/(dashboard)/layout.tsx
+++ apps/feel-ai/app/(dashboard)/layout.tsx
@@ -8,11 +8,22 @@ interface LayoutProps {
 export default function DashboardLayout({ children }: LayoutProps) {
   return (
     <div className="border-0 border-primary h-screen w-screen relative">
-      <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-secondary/75">
+      <aside className="absolute w-56 top-0 left-0 h-full border-r border-secondary/75">
         {/* @ts-expect-error RSC */}
         <Link href="/">Feelings</Link>
+        <div className="h-6"></div>
+        <ul className="menu bg-base-100 w-52 mt-5">
+          <li>
+            {/* @ts-expect-error RSC */}
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            {/* @ts-expect-error RSC */}
+            <Link href="/journal">Journal</Link>
+          </li>
+        </ul>
       </aside>
-      <div className="ml-[200px] h-full">
+      <div className="ml-56 h-full">
         <header className="relative h-[60px] border-b border-secondary/75">
           <div className="h-full w-full px-6 flex items-center justify-end">
             <UserButton />
