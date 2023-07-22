aa2006a8d40c543e474cc0fe7e40c469756fe0c8
Rade58 <bajic.rade2@gmail.com>
2023-07-22 15:30:48 +0200

    feat(feel-ai app) chart

--- apps/feel-ai/app/(dashboard)/history/page.tsx
+++ apps/feel-ai/app/(dashboard)/history/page.tsx
@@ -1,7 +1,45 @@
-export default function HistoryPage() {
+import HistoryChart from "@/components/HistoryChart";
+import { prisma } from "@/lib/db";
+import { getUserByClerkId } from "@/util/auth";
+
+async function getData() {
+  const user = await getUserByClerkId({});
+
+  const analyses = await prisma.feelAnalysis.findMany({
+    where: {
+      userId: user.id,
+    },
+    // select: {
+    // sentimentScore: true,
+    // createdAt: true,
+    // updatedAt: true,
+    // },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  // sum of all scors of all analysis
+  const sum = analyses.reduce((acumm, { sentimentScore }) => {
+    return acumm + sentimentScore;
+  }, 0);
+
+  const average = Math.round(sum / analyses.length);
+
+  return { analyses, average };
+}
+
+export default async function HistoryPage() {
+  const { analyses, average } = await getData();
+
+  // console.log({ analyses });
+
   return (
-    <div>
-      <h1>History</h1>
+    <div className="h-full px-6 py-8">
+      <h1 className="text-2xl text-secondary p-8">History: {average}</h1>
+      <div className="w-full h-full">
+        <HistoryChart analyses={analyses} />
+      </div>
     </div>
   );
 }
---
+++ apps/feel-ai/components/HistoryChart.tsx
@@ -0,0 +1,65 @@
+"use client";
+import type { FeelAnalysis } from "db_two";
+import { ResponsiveContainer, LineChart, Line, XAxis, Tooltip } from "recharts";
+
+export default function HistoryChart({
+  analyses,
+}: {
+  analyses: FeelAnalysis[];
+}) {
+  return (
+    // @ts-expect-error RSC
+    <ResponsiveContainer width="60%" height="50%">
+      {/* @ts-expect-error RSC */}
+      <LineChart width={200} height={50} data={analyses}>
+        {/* @ts-expect-error RSC */}
+        <Line
+          type="monotone"
+          dataKey="sentimentScore"
+          stroke="red"
+          strokeWidth={2}
+          activeDot={{ r: 8 }}
+        />
+        <XAxis dataKey="updatedAt" />
+        {/* @ts-ignore */}
+        <Tooltip content={<CustomTooltip />} />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+}
+
+function CustomTooltip({
+  payload,
+  label,
+  active,
+}: {
+  active: boolean;
+  label: string;
+  payload: { payload: FeelAnalysis }[];
+}) {
+  const dateLabel = new Date(label).toLocaleString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+
+  if (active) {
+    const analysis = payload[0].payload;
+
+    return (
+      <div className="p-8 custom-tooltip bg-info/60 shadow-md text-base-content border-secondary/20 rounded-lg backdrop-blur-md relative">
+        <div
+          className="absolute left-2 top-2 w-2 h-2 rounded-full"
+          style={{ background: analysis.color }}
+        ></div>
+        <p className="label text-sm text-info/30">{dateLabel}</p>
+        <p className="intro text-xl uppercase">{analysis.mood}</p>
+      </div>
+    );
+  }
+
+  return null;
+}
