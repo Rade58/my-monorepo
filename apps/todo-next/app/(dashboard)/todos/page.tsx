7afe395c3f41ec88fd9d94793615ff027ba9a0ba
Rade58 <bajic.rade2@gmail.com>
2023-07-25 18:21:04 +0200

    feat(todo-next) querying todos

--- apps/todo-next/app/(dashboard)/todos/page.tsx
+++ apps/todo-next/app/(dashboard)/todos/page.tsx
@@ -1,3 +1,22 @@
-export default function TodosPage() {
-  return <div>Todos Page</div>;
+import db from '@/lib/db';
+
+async function getData() {
+  const todos = await db.todo.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return todos;
+}
+
+export default async function TodosPage() {
+  const todos = await getData();
+
+  return (
+    <div>
+      <h1 className=" text-3xl text-primary">Todos Page</h1>
+      <pre>{JSON.stringify({ todos }, null, 2)}</pre>
+    </div>
+  );
 }
